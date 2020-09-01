2e74f411275b84cd23229a10f48857fdcc9fc545
bruno alfred <alfredybruno@gmail.com>
2020-09-01 06:13:24 +0300

    removed front files

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer({ name }) {
-  return (
-    <footer className="container">
-      <p>© { name || 'Company'} 2017-2020</p>
-    </footer>
-  );
-}
