44e92780218bd7fa885c6314fa2ea0f7fa4d224d
bruno alfred <alfredybruno@gmail.com>
2020-09-10 11:23:13 +0300

    feat: Sentry test route
    Ref #4

--- routes/sentry.js
+++ routes/sentry.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/debug-sentry", function mainHandler(req, res) {
+router.get("/", function mainHandler(req, res) {
     throw new Error("My first Sentry error!");
   });
 
