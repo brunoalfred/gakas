0898dd60210589b2dfaaab62247cd3dccc1cce5b
bruno alfred <alfredybruno@gmail.com>
2020-09-10 11:03:11 +0300

    fix: Config sentry
    Ref #4

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
+const express = require('express');
+const Sentry = require('@sentry/node');
+const Tracing = require("@sentry/tracing")
+
 require("dotenv").config();
 const DB_run = require("./lib/setup-db")
 
@@ -16,6 +20,25 @@ const inboxRouter = require("./routes/inbox");
 
 
 const app = express();
+
+
+Sentry.init({
+  dsn: "https://e4a8e7560ebd4c1288410ad6bb906735@o356272.ingest.sentry.io/5424154",
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -36,6 +59,19 @@ app.use("/inbox", inboxRouter);
 
 // Connect to DB
 DB_run()
+
+
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
+});
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
